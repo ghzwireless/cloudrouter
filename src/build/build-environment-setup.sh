d9fe0432e69f4b8505c1336009fa6aed7b0163d0
Arun Babu Neelicattu <aneelicattu@iix.net>
2015-02-20 10:54:00 +1000

    [build] Update build environment script to install jenkins
    Signed-off-by: Arun Babu Neelicattu <aneelicattu@iix.net>

--- src/build/build-environment-setup.sh
+++ src/build/build-environment-setup.sh
@@ -2,9 +2,32 @@
 
 # ensure we got everything we need to start building images
 yum --assumeyes install \
-    wget curl gnupg python git genisoimage \
+    deltarpm wget curl gnupg python git genisoimage \
     qemu-kvm qemu-img virt-manager libvirt libvirt-python \
     libvirt-client net-tools libguestfs-tools
 
 # import the fedora gpg key to be used for image verification
 curl https://getfedora.org/static/fedora.gpg | gpg --import
+
+# setup jenkins
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
+
+yum --assumeyes install java jenkins firewalld
+
+# configure firewall
+systemctl start firewalld
+firewall-cmd --permanent --zone=public --add-port=8080/tcp
+firewall-cmd --permanent --zone=public --add-port=22/tcp
+firewall-cmd --reload
+
+# fire up jenkins
+chkconfig jenkins on
+systemctl start jenkins
+
+# ensure we are all up-to-date
+yum --assumeyes update
+
+# TODO: FIX
+# PoC hack to allow jenkins to run as root for virsh magic
+echo "jenkins   ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
