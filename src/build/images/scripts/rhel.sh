e29c54bc7b221bfc7280f5322562533a49fc0ac0
Arun Babu Neelicattu <abn@iix.net>
2015-03-03 05:16:40 +1100

    [image] Add script for rhel to install epel
    Signed-off-by: Arun Babu Neelicattu <abn@iix.net>

---
+++ src/build/images/scripts/rhel.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+EPEL_URL_PREFIX=http://dl.fedoraproject.org/pub/epel/7/x86_64/e/
+
+# install any requirements
+yum -y install curl
+
+RPM_NAME=$(curl --silent ${EPEL_URL_PREFIX} \
+    | egrep -oh "epel-release-7-[0-9]*.noarch.rpm" \
+    | sort -u | head -n 1)
+
+yum -y ${EPEL_URL_PREFIX}${RPM_NAME}
