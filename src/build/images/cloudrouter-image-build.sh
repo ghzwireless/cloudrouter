8a4c89641671e1ef92601bf0721159d9d61f89e2
Arun Babu Neelicattu <abn@iix.net>
2015-03-03 04:36:31 +1100

    [image] Extra args should be run first
    Signed-off-by: Arun Babu Neelicattu <abn@iix.net>

--- src/build/images/cloudrouter-image-build.sh
+++ src/build/images/cloudrouter-image-build.sh
@@ -80,9 +80,9 @@ echo ""
 ${VIRT_BUILDER_CMD} ${BUILDER} \
     --arch ${ARCH} \
     --root-password locked:disabled \
+    ${BUILD_EXTRA_ARGS} \
     --run-command "yum -y install ${RELEASE_RPM}" \
     --run-command "rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CLOUDROUTER" \
-    ${BUILD_EXTRA_ARGS} \
     --install "${PACKAGES}" \
     --update \
     --format raw \
