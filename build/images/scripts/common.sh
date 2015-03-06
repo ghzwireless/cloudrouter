44d5fa478ed34d4bbb6fccff995c7a864790bd79
David Jorm <david.jorm@gmail.com>
2015-03-06 16:48:19 +1000

    Added cloudrouter-system-config package, removed sysctl conf from build script
    Signed-off-by: David Jorm <david.jorm@gmail.com>

--- build/images/scripts/common.sh
+++ build/images/scripts/common.sh
@@ -1,9 +1,2 @@
 #!/usr/bin/env bash
-
-SYSCTL_CONF="/etc/sysctl.d/90-cloudrouter-default.conf"
-
-cat > ${SYSCTL_CONF} << EOF
-net.ipv4.ip_forward = 1
-net.ipv6.conf.all.forwarding = 1
-net.ipv6.route.max_size = 50000
-EOF
+# This is a stub script, nothing here yet
