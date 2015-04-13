24aee2ad54b6b6d59024c19d70d73f116168a739
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2015-04-14 07:31:39 +1000

    [osv] Add packages required to build opendaylight appliance
    Signed-off-by: Arun Babu Neelicattu <arun.neelicattu@gmail.com>

--- build/osv/host/configure-centos-7.sh
+++ build/osv/host/configure-centos-7.sh
@@ -5,4 +5,6 @@ VBOX_REPO=http://download.virtualbox.org/virtualbox/rpm/rhel/virtualbox.repo
 curl -o /etc/yum.repos.d/$(basename ${VBOX_REPO}) ${VBOX_REPO}
 yum -y install \
     VirtualBox-* \
-    libstdc++ libstdc++-static
+    libstdc++ libstdc++-static \
+    boost-devel \
+    maven
