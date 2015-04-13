4b5ffefb87d7ba26e195a5616e1443228f895fae
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2015-04-14 07:42:13 +1000

    [osv] Add qemu-system-x86 to host package list
    Signed-off-by: Arun Babu Neelicattu <arun.neelicattu@gmail.com>

--- build/osv/host/configure-centos-7.sh
+++ build/osv/host/configure-centos-7.sh
@@ -4,6 +4,7 @@ VBOX_REPO=http://download.virtualbox.org/virtualbox/rpm/rhel/virtualbox.repo
 
 curl -o /etc/yum.repos.d/$(basename ${VBOX_REPO}) ${VBOX_REPO}
 yum -y install \
+    qemu-system-x86 \
     VirtualBox-* \
     libstdc++ libstdc++-static \
     boost-devel \
