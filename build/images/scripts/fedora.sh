7614baf9b93927060fc0beab906f17c2dd6b8552
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2015-04-13 14:20:16 +1000

    [build/images] Remove branding in fedora images. CloudRouter Release package now handles /etc/issue changes.
    Signed-off-by: Arun Babu Neelicattu <arun.neelicattu@gmail.com>

--- build/images/scripts/fedora.sh
+++ build/images/scripts/fedora.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-for ISSUE in "/etc/issue" "/etc/issue.net"; do
-    [[ -f ${ISSUE} ]] && sed -i \
-        's/Fedora.*/CloudRouter 1.0 Beta based on Fedora/' \
-        ${ISSUE}
-done
+# remove all feodra branding
+rpm -qa | grep '^fedora-' | xargs -I {} rpm -e --nodeps {}
 
