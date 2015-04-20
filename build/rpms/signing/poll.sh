58e2e9c0c4f4f33f9d3946d425ca5b94f7518556
David Jorm <david.jorm@gmail.com>
2015-04-20 19:09:45 +1000

    Added email notification
    Signed-off-by: David Jorm <david.jorm@gmail.com>

--- build/rpms/signing/poll.sh
+++ build/rpms/signing/poll.sh
@@ -4,24 +4,30 @@ BUILD_SERVER_HOST=104.197.15.26
 BUILD_SERVER_PORT=8443
 BUILD_SERVER_API_PATH=/job/cloudrouter-build-rpms/lastSuccessfulBuild/api/json
 BUILD_SERVER_URL=${BUILD_SERVER_PROTO}://${BUILD_SERVER_HOST}:${BUILD_SERVER_PORT}${BUILD_SERVER_API_PATH}
-CURL_CMD="curl --insecure"
+CURL_CMD="curl --insecure -s"
+CR_SIGNING_DIR=~/.crsigning
+NOTIFICATION_EMAIL=djorm@iix.net
 
-if [ ! -d ~/.crsigning ] ; then
+if [ ! -d ${CR_SIGNING_DIR} ] ; then
 	# First run, setup...
 	echo "First run"
-	mkdir ~/.crsigning
-	${CURL_CMD} ${BUILD_SERVER_URL} | jq '.number' > ~/.crsigning/lastBuild
+	mkdir ${CR_SIGNING_DIR}
+	${CURL_CMD} ${BUILD_SERVER_URL} | jq '.number' > ${CR_SIGNING_DIR}/lastBuild
 fi
 
 # Compare last locally known build to last successful remote build
 LAST_BUILD_LOCAL=`cat ~/.crsigning/lastBuild`
-${CURL_CMD} ${BUILD_SERVER_URL} | jq '.number' > ~/.crsigning/lastBuild
+${CURL_CMD} ${BUILD_SERVER_URL} | jq '.number' > ${CR_SIGNING_DIR}/lastBuild
 LAST_BUILD_REMOTE=`cat ~/.crsigning/lastBuild`
 if [ ${LAST_BUILD_REMOTE} -gt ${LAST_BUILD_LOCAL} ] ; then
 	# New build available, download all the artifacts
 	BUILD_URL=`${CURL_CMD} ${BUILD_SERVER_URL} | jq -r '.url'`
+	mkdir ${CR_SIGNING_DIR}/${LAST_BUILD_REMOTE}
 	${CURL_CMD} ${BUILD_SERVER_URL} | jq -r '.artifacts[]' | grep fileName | while read -r line; do
 		FILE_NAME=`echo ${line} | cut -d '"' -f4`
-		${CURL_CMD} "${BUILD_URL}/artifact/${FILE_NAME}" -o ${FILE_NAME}
+		${CURL_CMD} "${BUILD_URL}/artifact/${FILE_NAME}" -o ${CR_SIGNING_DIR}/${LAST_BUILD_REMOTE}/${FILE_NAME}
+		echo "Downloaded artifact ${CR_SIGNING_DIR}/${LAST_BUILD_REMOTE}/${FILE_NAME}"
 	done
+	SIGNING_TARGETS=`ls ${CR_SIGNING_DIR}/${LAST_BUILD_REMOTE}/*.rpm`
+	echo "The following artifacts have been downloaded, ready for signing: ${SIGNING_TARGETS}" | mail ${NOTIFICATION_EMAIL}
 fi
